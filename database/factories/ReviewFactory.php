1fefeda74a062123cf6474f98c3e78460717bc0b
aliharby12 <aliharby565@gmail.com>
2020-11-02 11:56:54 +0200

    create the seeds for products and reviews

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -3,10 +3,14 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Models\Review;
+use App\Models\Product;
 use Faker\Generator as Faker;
 
 $factory->define(Review::class, function (Faker $faker) {
     return [
-        //
+        'product_id' => App\models\Product::all()->random()->id,
+        'customer' => $faker->name,
+        'review' => $faker->paragraph,
+        'star' => $faker->numberBetween(0, 5),
     ];
 });
