83eb32e88de8e7c58a5fad66f8e91a9e04494cd8
aliharby12 <aliharby565@gmail.com>
2020-12-25 20:43:43 +0200

    edit the product resource

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -19,8 +19,11 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->description,
             'price' => $this->price,
-            'stock' => $this->stock,
+            'stock' => $this->stock == 0 ? "Out Of Stock" : $this->stock,
             'discount' => $this->discount,
+            'Price After Discount' => round((1-($this->discount/100)) * $this->price, 2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : "No Rating Yet",
+            'link' => ['reviews' => route('reviews.index', $this->id)]
         ];
     }
 }
